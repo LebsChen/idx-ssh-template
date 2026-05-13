ff4faac87caa984149ac9230971d26ba1998be61
LebsChen <openclaw@openclaw-02.localhost>
2026-05-13 15:58:44 +0000

    Use IP instead of domain for SISH_HOST default

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-SISH_HOST="${SISH_HOST:-idx.yaoshen.de5.net}"
+SISH_HOST="${SISH_HOST:-117.31.178.161}"
 SISH_PORT="${SISH_PORT:-2022}"
 SISH_USER="${SISH_USER:-user}"
 SISH_ALIAS="${SISH_ALIAS:-${GOOGLE_CLOUD_WORKSTATION_NAME:-$(cat /etc/hostname 2>/dev/null | tr -d '[:space:]' || hostname 2>/dev/null)}}"
